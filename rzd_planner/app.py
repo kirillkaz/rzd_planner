f6d4824c3f5b56c65d51119624b0cbbda217a9c1
kirillkaz <kirillkazazaev@gmail.com>
2025-03-30 01:03:32 +1000

    feat(app.py) added migrations on start app

--- rzd_planner/app.py
+++ rzd_planner/app.py
@@ -1,11 +1,21 @@
 import dash_bootstrap_components as dbc
 from dash_extensions.enrich import DashProxy, MultiplexerTransform
 from flask import Flask
+from flask_migrate import Migrate
 
+from rzd_planner.config import config
+from rzd_planner.models import db
+import flask_migrate
 
 def init_app() -> DashProxy:
     """Функция для создания инстанса приложения"""
     server = Flask(__name__)
+    server.config.from_object(config)
+    db.init_app(server)
+    Migrate().init_app(server, db)
+    with server.app_context():
+        flask_migrate.upgrade(directory="migrations/")
+
     return DashProxy(
         transforms=[MultiplexerTransform()],
         external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME],
