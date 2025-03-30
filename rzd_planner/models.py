8a291aa1cc88e8e704e771ea657b2782c40f9490
kirillkaz <kirillkazazaev@gmail.com>
2025-03-30 19:49:06 +1000

    feat(models.py)
    Added trains model
    Added stations model
    Added train_types model
    Added train_travel_times model
    Added train_routes model

--- rzd_planner/models.py
+++ rzd_planner/models.py
@@ -1,8 +1,23 @@
-from uuid import UUID
+from __future__ import annotations
+
+import uuid
+from datetime import datetime
+from enum import StrEnum
 
 from flask_sqlalchemy import SQLAlchemy
 from passlib.context import CryptContext
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import CheckConstraint, ForeignKey, String, UniqueConstraint
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class OnDeleteActionType(StrEnum):
+    """Тип для событий, происходящих при удалении продительской записи"""
+
+    CASCADE: str = "CASCADE"
+    SET_NULL: str = "SET NULL"
+    RESTRICT: str = "RESTRICT"
+    NO_ACTION: str = "NO ACTION"
+    SET_DEFAULT: str = "SET_DEFAULT"
 
 
 class Base(DeclarativeBase):
@@ -31,8 +46,8 @@ class User(db.Model):
 
     __tablename__ = "users"
 
-    id: Mapped[UUID] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(unique=True)
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid1)
+    username: Mapped[str] = mapped_column(String(30), unique=True)
     password_hash: Mapped[str] = mapped_column()
 
     @property
@@ -47,3 +62,155 @@ class User(db.Model):
     def verify_password(self, password: str) -> bool:
         """Проверяет, соответствует ли пароль хэшу."""
         return _pwd_context.verify(password, self.password_hash)
+
+
+class TrainTypes(db.Model):
+    """Модель типов поездов"""
+
+    __tablename__ = "train_types"
+
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid1)
+    name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    min_speed: Mapped[float] = mapped_column(nullable=False)
+    max_speed: Mapped[float] = mapped_column()
+    min_distance: Mapped[float] = mapped_column(nullable=False)
+    max_distance: Mapped[float] = mapped_column()
+
+    trains: Mapped[list["Trains"]] = relationship(back_populates="train_type")
+
+    __table_args__ = (
+        CheckConstraint(
+            "min_speed > 0.0",
+            name="min_speed_range_constraint",
+        ),
+        CheckConstraint(
+            "max_speed > min_speed",
+            name="max_speed_range_constraint",
+        ),
+        CheckConstraint(
+            "min_speed > 0.0",
+            name="min_distance_range_constraint",
+        ),
+        CheckConstraint(
+            "max_speed > min_distance",
+            name="max_distance_range_constraint",
+        ),
+    )
+
+
+class Trains(db.Model):
+    """Модель поездов"""
+
+    __tablename__ = "trains"
+
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid1)
+    train_number: Mapped[str] = mapped_column(String(5), unique=True, nullable=False)
+
+    train_type_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey(
+            "train_types.id",
+            ondelete=str(OnDeleteActionType.CASCADE),
+        ),
+        nullable=False,
+    )
+
+    train_type: Mapped["TrainTypes"] = relationship(back_populates="trains")
+
+
+class TrainTravelTimes(db.Model):
+    """Модель времени поездок на маршрутах"""
+
+    __tablename__ = "train_travel_times"
+
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True)
+    start_date: Mapped[datetime] = mapped_column(nullable=False)
+    end_date: Mapped[datetime] = mapped_column(nullable=False)
+
+    train_route_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey(
+            "train_routes.id",
+            ondelete=str(OnDeleteActionType.CASCADE),
+        ),
+        nullable=False,
+    )
+
+    train_route: Mapped["TrainRoutes"] = relationship(
+        back_populates="train_travel_times"
+    )
+
+    __table_args__ = (
+        CheckConstraint(
+            "start_date != end_date",
+            name="dates_not_equal_constraint",
+        ),
+        CheckConstraint(
+            "start_date < end_date",
+            name="end_date_higher_start_date_constraint",
+        ),
+    )
+
+
+class TrainRoutes(db.Model):
+    """Модель маршрутов поездок"""
+
+    __tablename__ = "train_routes"
+
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True)
+    distance: Mapped[float] = mapped_column(nullable=False)
+
+    start_station_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey(
+            "stations.id",
+            ondelete=str(OnDeleteActionType.CASCADE),
+        ),
+        nullable=False,
+    )
+    end_station_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey(
+            "stations.id",
+            ondelete=str(OnDeleteActionType.CASCADE),
+        ),
+        nullable=False,
+    )
+
+    start_station: Mapped["Stations"] = relationship(
+        back_populates="routes_as_start",
+        foreign_keys=[start_station_id],
+    )
+    end_station: Mapped["Stations"] = relationship(
+        back_populates="routes_as_end",
+        foreign_keys=[end_station_id],
+    )
+    train_travel_times: Mapped[list["TrainTravelTimes"]] = relationship(
+        back_populates="train_route"
+    )
+
+    __table_args__ = (
+        CheckConstraint(
+            "start_station_id != end_station_id", name="stations_not_equal_constraint"
+        ),
+        CheckConstraint("distance > 0.0", name="distance_range_constraint"),
+        UniqueConstraint(
+            "start_station_id",
+            "end_station_id",
+            name="start_end_stations_unique_constraint",
+        ),
+    )
+
+
+class Stations(db.Model):
+    """Модель станций"""
+
+    __tablename__ = "stations"
+
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+
+    routes_as_start: Mapped[list["TrainRoutes"]] = relationship(
+        back_populates="start_station",
+        foreign_keys="[TrainRoutes.start_station_id]",
+    )
+    routes_as_end: Mapped[list["TrainRoutes"]] = relationship(
+        back_populates="end_station",
+        foreign_keys="[TrainRoutes.end_station_id]",
+    )
