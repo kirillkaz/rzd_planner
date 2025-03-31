0783b0b188988b324bc9efc1e05a630e3d9f00da
kirillkaz <kirillkazazaev@gmail.com>
2025-03-31 23:38:45 +1000

    feat(main_layout.py) added locations and page content elem

--- rzd_planner/layouts/main_layout.py
+++ rzd_planner/layouts/main_layout.py
@@ -1,7 +1,10 @@
-from dash_extensions.enrich import html
-
+from dash_extensions.enrich import html, dcc
 
 def render_main_layout() -> html.Div:
     """Рендер главной страницы"""
 
-    return html.Div()
+    return html.Div([
+        dcc.Location(id="url", refresh=False),
+        dcc.Location(id="redirect", refresh=True),
+        html.Div(id="page-content"),
+    ])
