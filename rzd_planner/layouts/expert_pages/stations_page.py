e6106cbcb458b99b95bc25a9a2ab0d13bd752025
kirillkaz <kirillkazazaev@gmail.com>
2025-04-02 01:42:57 +1000

    feat(stations_page.py) Added modal for stations page

--- rzd_planner/layouts/expert_pages/stations_page.py
+++ rzd_planner/layouts/expert_pages/stations_page.py
@@ -1,9 +1,44 @@
 import dash_bootstrap_components as dbc
-from dash_extensions.enrich import html
+from dash_extensions.enrich import dcc, html
 
 from rzd_planner.layouts.components.header import render_expert_header
 from rzd_planner.layouts.components.table_component import render_table
 from rzd_planner.layouts.components.table_header import render_table_header
+
+
+def _render_modal() -> dbc.Modal:
+    """Модальное окно для сохранения станций"""
+    return dbc.Modal(
+        children=[
+            dbc.ModalHeader(dbc.ModalTitle("Добавление станции")),
+            dbc.ModalBody(
+                children=[
+                    html.Div(
+                        children=[
+                            html.P("Название станции"),
+                            dbc.Input(
+                                id="stations-name-id",
+                            ),
+                        ],
+                        className="stations-name-block",
+                    ),
+                    html.Div(
+                        children=[
+                            dbc.Button(
+                                children="Сохранить",
+                                id="stations-save-btn-id",
+                            ),
+                        ],
+                        className="stations-save-btn-block",
+                    ),
+                ],
+                className="stations-modal-body",
+            ),
+        ],
+        id="stations-modal-id",
+        is_open=True,
+        centered=True,
+    )
 
 
 def render_stations_page() -> html.Div:
@@ -14,6 +49,7 @@ def render_stations_page() -> html.Div:
 
     return html.Div(
         children=[
+            _render_modal(),
             render_expert_header(),
             html.Div(
                 children=[
@@ -34,6 +70,7 @@ def render_stations_page() -> html.Div:
                                 children="Удалить станции",
                                 color="secondary",
                                 id="stations-delete-btn-id",
+                                disabled=True,
                             ),
                         ],
                     ),
@@ -41,5 +78,6 @@ def render_stations_page() -> html.Div:
                 ],
                 className="table-block",
             ),
+            dcc.Store(id="stations-table-upload-trigger", storage_type="memory"),
         ],
     )
