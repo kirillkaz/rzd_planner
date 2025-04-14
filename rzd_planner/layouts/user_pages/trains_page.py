9aa0441558fff7fc9f5023dfe66db67e40dbf479
kirillkaz <kirillkazazaev@gmail.com>
2025-04-15 00:58:10 +1000

    feat(trains_page.py) added deletion and adding trains

--- rzd_planner/layouts/user_pages/trains_page.py
+++ rzd_planner/layouts/user_pages/trains_page.py
@@ -1,9 +1,55 @@
 import dash_bootstrap_components as dbc
-from dash_extensions.enrich import html
+from dash_extensions.enrich import dcc, html
 
+from rzd_planner.layouts.components.del_modal_component import render_delete_modal
 from rzd_planner.layouts.components.header import render_user_header
 from rzd_planner.layouts.components.table_component import render_table
 from rzd_planner.layouts.components.table_header import render_table_header
+
+
+def _render_modal() -> dbc.Modal:
+    """Функция для отрисовки модального окна"""
+    return dbc.Modal(
+        children=[
+            dbc.ModalHeader(dbc.ModalTitle("Добавление поезда")),
+            dbc.ModalBody(
+                children=[
+                    html.Div(
+                        children=[
+                            html.Div(
+                                children=[
+                                    html.P("Номер поезда:"),
+                                    dbc.Input(id="train-input-id"),
+                                ],
+                                className="modal-train-block",
+                            ),
+                            html.Div(
+                                children=[
+                                    html.P("Тип поезда:"),
+                                    dbc.Select(id="train-type-select-id"),
+                                ],
+                                className="modal-train-type-select-block",
+                            ),
+                        ],
+                        className="train-modal-body-inner",
+                    ),
+                    html.Div(
+                        children=[
+                            dbc.Button(
+                                children="Сохранить",
+                                id="train-save-btn-id",
+                            ),
+                        ],
+                        className="train-save-btn-block",
+                    ),
+                ],
+                className="train-modal-body",
+            ),
+        ],
+        id="train-modal-id",
+        is_open=False,
+        centered=True,
+    )
 
 
 def render_trains_page() -> html.Div:
@@ -17,6 +63,12 @@ def render_trains_page() -> html.Div:
 
     return html.Div(
         children=[
+            render_delete_modal(
+                "Удаление поездов",
+                "trains-delete-confirm-btn-id",
+                "trains-delete-modal-id",
+            ),
+            _render_modal(),
             render_user_header(),
             html.Div(
                 children=[
@@ -44,5 +96,6 @@ def render_trains_page() -> html.Div:
                 ],
                 className="table-block",
             ),
+            dcc.Store("trains-table-upload-trigger", storage_type="memory", data={}),
         ],
     )
