980288316d8462ff440c09f0fb919f82ce1cba33
kirillkaz <kirillkazazaev@gmail.com>
2025-04-16 01:01:49 +1000

    feat(routes_page.py) added modals

--- rzd_planner/layouts/user_pages/routes_page.py
+++ rzd_planner/layouts/user_pages/routes_page.py
@@ -1,9 +1,48 @@
 import dash_bootstrap_components as dbc
-from dash_extensions.enrich import html
+from dash_extensions.enrich import dcc, html
 
+from rzd_planner.layouts.components.del_modal_component import render_delete_modal
 from rzd_planner.layouts.components.header import render_user_header
 from rzd_planner.layouts.components.table_component import render_table
 from rzd_planner.layouts.components.table_header import render_table_header
+
+
+def _render_modal() -> dbc.Modal:
+    """Функция для отрисовки модального окна"""
+    return dbc.Modal(
+        children=[
+            dbc.ModalHeader(dbc.ModalTitle("Добавление маршрута")),
+            dbc.ModalBody(
+                children=[
+                    html.Div(
+                        children=[
+                            html.Div(
+                                children=[
+                                    html.P("Маршрут:"),
+                                    dbc.Select(id="user-routes-select-id"),
+                                ],
+                                className="modal-user-routes-block",
+                            ),
+                        ],
+                        className="user-routes-modal-body-inner",
+                    ),
+                    html.Div(
+                        children=[
+                            dbc.Button(
+                                children="Сохранить",
+                                id="user-routes-save-btn-id",
+                            ),
+                        ],
+                        className="user-routes-save-btn-block",
+                    ),
+                ],
+                className="user-routes-modal-body",
+            ),
+        ],
+        id="user-routes-modal-id",
+        is_open=False,
+        centered=True,
+    )
 
 
 def render_user_routes_page() -> html.Div:
@@ -14,6 +53,12 @@ def render_user_routes_page() -> html.Div:
 
     return html.Div(
         children=[
+            render_delete_modal(
+                title="Удаление маршрута",
+                button_id="user-routes-delete-confirm-btn-id",
+                modal_id="user-routes-delete-modal-id",
+            ),
+            _render_modal(),
             render_user_header(),
             html.Div(
                 children=[
@@ -42,6 +87,9 @@ def render_user_routes_page() -> html.Div:
                     ),
                 ],
                 className="table-block",
+            ),
+            dcc.Store(
+                "routes-user-table-upload-trigger", storage_type="memory", data={}
             ),
         ],
     )
