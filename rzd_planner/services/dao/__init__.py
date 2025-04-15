eb1329c976d2c63670df138f735e62b148bbe6c6
kirillkaz <kirillkazazaev@gmail.com>
2025-04-16 00:59:15 +1000

    feat(full_routes_dao.py) Added full routes dao

--- rzd_planner/services/dao/__init__.py
+++ rzd_planner/services/dao/__init__.py
@@ -1,4 +1,5 @@
 from .expert_routes_dao import ExpertRouteDTO, ExpertRoutesDAO
+from .full_routes_dao import FullRoutesDAO, FullRoutesDTO
 from .stations_dao import StationsDAO, StationsDTO
 from .train_type_dao import TrainTypeDAO, TrainTypeDTO
 from .trains_dao import TrainDAO, TrainDTO
---
+++ rzd_planner/services/dao/full_routes_dao.py
@@ -0,0 +1,49 @@
+from dataclasses import asdict, dataclass
+from typing import Self
+
+from rzd_planner.models import FullRoutes, db
+
+
+@dataclass
+class FullRoutesDTO:
+    """DTO полных маршрутов"""
+
+    route: str
+
+
+class FullRoutesDAO:
+    """DAO полных маршрутов"""
+
+    def save(self: Self, dto: FullRoutesDTO) -> None:
+        """Метод для сохранения полных маршрутов
+
+        Args:
+            dto (FullRoutesDTO): DTO полных маршрутов
+        """
+        with db.session() as session:
+            obj = FullRoutes(**asdict(dto))
+            session.add(obj)
+            session.commit()
+
+    def get_all(self: Self) -> list[FullRoutes]:
+        """Метод для извлечения всех полных маршрутов из БД
+
+        Returns:
+            list[FullRoutes]: типы поездов
+        """
+        with db.session() as session:
+            models = session.query(FullRoutes).all()
+
+        return models
+
+    def delete(self: Self, uuid_lst: list[str]) -> None:
+        """Метод для удаления полных маршрутов
+
+        Args:
+            uuid_lst (list[str]): список uuid записей
+        """
+        with db.session() as session:
+            session.query(FullRoutes).filter(FullRoutes.id.in_(uuid_lst)).delete(
+                synchronize_session=False
+            )
+            session.commit()
