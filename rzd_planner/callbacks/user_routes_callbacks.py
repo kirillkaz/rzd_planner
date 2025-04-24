0c873f95e4ea1548e69c4494e8738c494e9fe38d
kirillkaz <kirillkazazaev@gmail.com>
2025-04-25 01:02:09 +1000

    feat(user_routes_callbacks.py) added callbacks for trains field and distance field

--- rzd_planner/callbacks/user_routes_callbacks.py
+++ rzd_planner/callbacks/user_routes_callbacks.py
@@ -4,12 +4,19 @@ from dash_extensions.enrich import Input, Output, State, no_update
 from sqlalchemy.exc import SQLAlchemyError
 
 from rzd_planner.app import app
-from rzd_planner.services.dao import ExpertRoutesDAO, FullRoutesDAO, FullRoutesDTO
+from rzd_planner.services.dao import (
+    ExpertRoutesDAO,
+    FullRoutesDAO,
+    FullRoutesDTO,
+    TrainDAO,
+)
 from rzd_planner.services.mappers import (
     FullRouteOption,
     FullRoutesMapper,
     FullRoutesTableType,
+    TrainsMapper,
 )
+from rzd_planner.services.routes_distance_calc_service import RoutesDistanceCalc
 from rzd_planner.services.train_routes_service import TrainRoutesService
 
 
@@ -42,10 +49,38 @@ def upload_usr_routes_stations(_: int) -> list[FullRouteOption]:
     """
     objs = ExpertRoutesDAO().get_all()
     raw_options = TrainRoutesService().get_train_routes(objs)
-    print(raw_options)
     options = FullRoutesMapper().str_to_options(raw_options)
-    print(options)
     return options
+
+
+@app.callback(
+    Output("user-routes-select-train-id", "options"),
+    Output("user-routes-distance-id", "invalid"),
+    Input("user-routes-distance-id", "value"),
+    prevent_initial_call=True,
+)
+def upload_usr_routes_trains(distance: int) -> tuple[list[FullRouteOption], bool]:
+    """Колбэк для предзагрузки селекторов с поездами
+
+    Returns:
+        tuple[list[FullRouteOption], bool]: поезда + инвалидность поля
+    """
+    objs = TrainDAO().get_all_free_by_distance_not(distance)
+    options = TrainsMapper().model_to_options(objs)
+    trains_is_invalid = not bool(options)
+
+    return options, trains_is_invalid
+
+
+@app.callback(
+    Output("user-routes-distance-id", "value"),
+    Input("user-routes-select-id", "value"),
+    prevent_initial_call=True,
+)
+def upload_distance_field(route: str) -> float:
+    """Колбэк для обновления дистанции поездки для маршрута"""
+
+    return RoutesDistanceCalc().calc_distance(route)
 
 
 @app.callback(
@@ -67,23 +102,31 @@ def open_usr_routes_modal_callback(_: int) -> bool:
     inputs=dict(
         _=Input("user-routes-save-btn-id", "n_clicks"),
         route=State("user-routes-select-id", "value"),
+        train_id=State("user-routes-select-train-id", "value"),
+        distance=State("user-routes-distance-id", "value"),
     ),
     prevent_initial_call=True,
 )
 def save_usr_route_callback(
     _: int,
     route: str,
+    train_id: str,
+    distance: float,
 ) -> SaveUserRouteReturn:
     """Колбэк для сохранения полного маршрута
 
     Args:
         route (str): полный маршрут поездки
+        train_id (str): uuid поезда
+        distance (float): длина маршрута
 
     Returns:
         SaveUserRouteReturn: Структура возвращаемого типа для колбэка
     """
     dto = FullRoutesDTO(
         route=route,
+        train_id=train_id,
+        distance=distance,
     )
 
     route_invalid = not bool(route)
